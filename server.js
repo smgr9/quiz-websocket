8ee079c92cba5d4bf65be584e7e512921bd50b9d
smgr9 <30799257+smgr9@users.noreply.github.com>
2025-03-30 00:27:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,19 +9,21 @@ const PORT = 3000;
 
 // إعداد قاعدة البيانات
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root", // غيرها لو عندك يوزر مختلف
-  password: "", // ضع الباسورد لو عندك
-  database: "quiz_db",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
   if (err) {
-    console.error("❌  Database connection failed:", err);
+    console.error("❌ Database connection failed:", err);
     return;
   }
   console.log("✅ Database connected successfully!");
 });
+
 
 // إعداد WebSocket
 const wss = new WebSocket.Server({ port: 8080 });
